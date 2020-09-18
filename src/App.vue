438fd4f6f48249cb900a4fea7e1eff7e1dfea9a8
Filip Fedorišin <fedorisinfilip@gmail.com>
2020-09-18 15:02:54 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RegisterForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegisterForm from "./components/RegisterForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    RegisterForm,
+  },
+};
 </script>
 
 <style>
