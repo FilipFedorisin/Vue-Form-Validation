bfa40ffb72ca5eee56cc7e5b3cde3773567bc8ca
Filip Fedorišin <fedorisinfilip@gmail.com>
2020-09-18 15:03:32 +0200

    update

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="RegisterForm">
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  color: grey;
+}
+</style>
